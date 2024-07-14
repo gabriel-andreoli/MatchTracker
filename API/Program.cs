627a649dc529fdaf4e626eb64e1a4be31b7e8b06
Gabriel Andreoli <gabriel@marqponto.com.br>
2024-07-13 22:14:17 -0300

    FEAT - Adicionando Identity Framework para autenticação e autorização

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,8 @@
+using Infrastructure.Context;
+using Infrastructure.Identity.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//TODO Gabriel 20240713 - ExtensionMethods for this configurations
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication()
+    .AddBearerToken(IdentityConstants.BearerScheme);
+
+builder.Services.AddIdentityCore<User>()
+    .AddEntityFrameworkStores<MatchTrackerDbContext>()
+    .AddApiEndpoints();
+
+builder.Services.AddDbContext<MatchTrackerDbContext>(options => 
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection")));
 
 var app = builder.Build();
 
@@ -17,6 +35,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapIdentityApi<User>();
 
 app.UseAuthorization();
 
---
+++ Infrastructure/Context/MatchTrackerDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Infrastructure.Identity.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Context
+{
+    public sealed class MatchTrackerDbContext : IdentityDbContext<User,IdentityRole<Guid>, Guid>
+    {
+        public MatchTrackerDbContext(DbContextOptions<MatchTrackerDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.HasDefaultSchema("identity");
+        }
+    }
+}
